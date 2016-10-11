51a9fe7bf99c14545078d38a76307f05cdcd345f
jdraper9 <jamesdraper24@gmail.com>
2016-10-10 18:06:28 -0700

    Research String methods and use them to perform required changes on various strings

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,43 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+p "zom".replace "zoom"
+p "zom".gsub(/[o]/, 'oo')
+p "zom".insert(1, 'o')
+p "zom".insert(2, 'o')
 # => “zoom”
 
-# "enhance".<???>
+p "enhance".center(15)
+p "enhance".replace '    enhance    ' # Not going to use replace again after this, as it can be applied to any of the strings
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+p "the usual".concat(' suspects')
+p "the usual".insert(9, ' suspects')
+p "the usual" << ' suspects'
 #=> "the usual suspects"
 
-# " suspects".<???>
+p " suspects".prepend('the usual')
+p " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+p "The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+p "z".bytes
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z?) - ASCII equivalent
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count('a')
 # => 4
